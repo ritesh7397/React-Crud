f029b56161dbe129dcd5bff7109787eb37022185
Ritesh Nagrikar <riteshnagrikar77@gmail.com>
2025-09-05 19:25:04 +0530

    modified app.js anc index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Create from './Components/Create';
 import Update from './Components/Update';
@@ -11,13 +11,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/create' element={<Create/>}></Route>
-        <Route path='/update/:id' element={<Update/>}></Route>
-        <Route path='/read/:id' element={<Read/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/create' element={<Create />}></Route>
+        <Route path='/update/:id' element={<Update />}></Route>
+        <Route path='/read/:id' element={<Read />}></Route>
       </Routes>
     </BrowserRouter>
-    
+
   );
 }
 
